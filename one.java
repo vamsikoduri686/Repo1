6479d251336d712c62adce7d5516c67c67c66b83
koduruva <vamsi686@gmail.com>
2023-02-22 10:00:10 +0530

    a

---
+++ one.java
@@ -0,0 +1,5 @@
+Updates from Drak Dragons
+
+1) Wroked on integreation of dropdowns starting with imported enties and Add voilations Screen 
+2) worked on Search functionlity and integrated reasons with resons count and dedicated description box for other reasons.
+3) Impleted delete functionality for the Voilations screen 
